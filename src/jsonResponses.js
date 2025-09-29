01f9783db1fad5477ec8d15dec4ad57a46ba92ec
Gavin <111817327+gavinwebst@users.noreply.github.com>
2025-09-28 20:55:22 -0400

    finished the hw

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,76 +1,72 @@
 const users = {};
 
 const respondJSON = (request, response, status, object) => {
-    const content = JSON.stringify(object);
+  const content = JSON.stringify(object);
 
-    const headers = {
-        'Content-Type': 'applicaton/json',
-        'Content-Length': Buffer.byteLength(content, 'utf8'),
-    };
+  const headers = {
+    'Content-Type': 'applicaton/json',
+    'Content-Length': Buffer.byteLength(content, 'utf8'),
+  };
 
-    response.writeHead(status, headers);
+  response.writeHead(status, headers);
 
-    if(request.method !== 'HEAD' && status !== 204) {
-        response.write(content);
-    }
+  if (request.method !== 'HEAD' && status !== 204) {
+    response.write(content);
+  }
 
-    response.end();
-
-}
+  response.end();
+};
 
 const getUsers = (request, response) => {
-    const responseJSON = {
-        users,
-    };
+  const responseJSON = {
+    users,
+  };
 
-    return respondJSON(request, response, 200, responseJSON);
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const addUser = (request, response) => {
-    const responseJSON = {
-        message: 'Name and age are both required.',
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
+
+  const { name, age } = request.body;
+
+  if (!name || !age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 204;
+
+  if (!user[name]) {
+    responseCode = 201;
+    users[name] = {
+      name,
     };
+  }
 
-    const {name, age} = request.body;
+  users[name].age = age;
 
-    if(!name || !age) {
-        responseJSON.id = 'missingParams';
-        return respondJSON(request, response, 400, responseJSON);
-    }
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Succefully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
 
-    let responseCode = 204;
-
-    if(!user[name]) {
-        responseCode = 201;
-        users[name] = {
-            name: name,
-        };
-    }
-
-    users[name].age = age;
-
-    if(responseCode === 201) {
-        responseJSON.message = 'Created Succefully';
-        return respondJSON(request,response, responseCode, responseJSON);
-    }
-
-    return respondJSON(request, response, responseCode, {});
-}
-
-
+  return respondJSON(request, response, responseCode, {});
+};
 
 const notFound = (request, response) => {
-    const responseJSON = {
-        message: 'The page you are looking for was not found',
-        id: 'notFound',
-    };
+  const responseJSON = {
+    message: 'The page you are looking for was not found',
+    id: 'notFound',
+  };
 
-    respondJSON(request,response,404,responseJSON);
+  respondJSON(request, response, 404, responseJSON);
 };
 
 module.exports = {
-    getUsers,
-    addUser,
-    notFound,
+  getUsers,
+  addUser,
+  notFound,
 };
-
--- src/server.js
+++ src/server.js
@@ -1,66 +1,61 @@
 const http = require('http');
 const query = require('querystring');
+
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
-const { findPackageJSON } = require('module');
-
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-
 const parseBody = (request, response, handler) => {
-    const body = [];
+  const body = [];
 
-    request.on('error', (err) => {
-        console.dir(err);
-        response.statudCode = 400;
-        response.end();
-    });
+  request.on('error', (err) => {
+    console.dir(err);
+    response.statudCode = 400;
+    response.end();
+  });
 
-    request.on('data', (chunk) =>{
-        body.push[chunk];
-    });
+  request.on('data', (chunk) => {
+    body.push[chunk];
+  });
 
-    request.on('end', () => {
-        const bodyString = Buffer.concat(body).toString();
-        request.body = query.parse(bodyString);
+  request.on('end', () => {
+    const bodyString = Buffer.concat(body).toString();
+    request.body = query.parse(bodyString);
 
-        handler(request, response);
-    });
+    handler(request, response);
+  });
 };
 
 const handlePost = (request, response, parsedUrl) => {
-    if(parsedUrl.pathname === '/addUser') {
-        parseBody(request, response, jsonHandler.addUser);
-    }
+  if (parsedUrl.pathname === '/addUser') {
+    parseBody(request, response, jsonHandler.addUser);
+  }
 };
 
 const handleGET = (request, response, parsedUrl) => {
-    if(parsedUrl.pathname === '/style.css'){
-        htmlHandler.getCSS(request, response);
-    }else if(parsedUrl.pathname === '/getUsers') {
-        jsonHandler.getUsers(request, response);
-    }else if(parsedUrl.pathname ==='/'){
-        htmlHandler.getIndex(request,response);
-    } else{
-        jsonHandler.notFound(request, response);
-    }
+  if (parsedUrl.pathname === '/style.css') {
+    htmlHandler.getCSS(request, response);
+  } else if (parsedUrl.pathname === '/getUsers') {
+    jsonHandler.getUsers(request, response);
+  } else if (parsedUrl.pathname === '/') {
+    htmlHandler.getIndex(request, response);
+  } else {
+    jsonHandler.notFound(request, response);
+  }
 };
 
 const onRequest = (request, response) => {
+const protocol = request.connection.encrypted ? 'https' : 'http';  
+const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
 
-    const protocol = request.conntection.encrypted ? 'https' : 'http';
-    const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`);
-
-    if(request.method === 'POST') {
-        handlePost(request, response, parsedUrl);
-    } else {
-        handleGET(request, response, parsedUrl);
-    }
+  if (request.method === 'post') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGET(request, response, parsedUrl);
+  }
 };
 
-
-
 http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
+  console.log(`Listening on 127.0.0.1: ${port}`);
 });
